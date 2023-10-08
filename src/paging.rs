f3d0773974e9c625705de3b0646f2ff2177089f1
Alexander Zhang <alex@alexyzhang.dev>
2023-10-07 17:02:01 -0700

    Rewrite paging code to use bitfield
    The code is currently incomplete but stuff will be implemented as
    needed.

--- src/paging.rs
+++ src/paging.rs
@@ -3,7 +3,8 @@
 // Suppress dead code warnings during development.
 #![allow(dead_code)]
 
-use core::ops::{Index, IndexMut};
+use bitfield::bitfield;
+use core::ops::Index;
 
 const PAGE_OFFSET_BITS: i32 = 12;
 const PAGE_SIZE: usize = 1 << PAGE_OFFSET_BITS;
@@ -13,30 +14,56 @@ const VIRTUAL_ADDRESS_BITS: i32 = VIRTUAL_PAGE_NUMBER_BITS + PAGE_OFFSET_BITS;
 const PHYSICAL_PAGE_NUMBER_BITS: i32 = 44;
 const PHYSICAL_ADDRESS_BITS: i32 = PHYSICAL_PAGE_NUMBER_BITS + PAGE_OFFSET_BITS;
 
-#[derive(Debug, PartialEq, Eq, Clone, Copy)]
-pub struct PhysicalPageNumber(u64);
-
 #[derive(Debug)]
-pub struct InvalidPhysicalPageNumberError(u64);
+pub struct InvalidAddressError;
 
-impl TryFrom<u64> for PhysicalPageNumber {
-    type Error = InvalidPhysicalPageNumberError;
+#[derive(Debug, PartialEq, Eq, Clone, Copy)]
+pub struct PhysicalAddress(u64);
+
+impl TryFrom<u64> for PhysicalAddress {
+    type Error = InvalidAddressError;
 
     fn try_from(value: u64) -> Result<Self, Self::Error> {
-        if value < 1 << PHYSICAL_PAGE_NUMBER_BITS {
+        if value < 1 << PHYSICAL_ADDRESS_BITS {
             Ok(Self(value))
         } else {
-            Err(InvalidPhysicalPageNumberError(value))
+            Err(InvalidAddressError)
         }
     }
 }
 
-impl From<PhysicalPageNumber> for u64 {
-    fn from(value: PhysicalPageNumber) -> Self {
+impl From<PhysicalAddress> for u64 {
+    fn from(value: PhysicalAddress) -> Self {
         value.0
     }
 }
 
+impl PhysicalAddress {
+    pub fn page_offset(self) -> u64 {
+        self.0 & ((1 << PAGE_OFFSET_BITS) - 1)
+    }
+    pub fn is_page_aligned(self) -> bool {
+        self.page_offset() == 0
+    }
+}
+
+/// A page-aligned physical address.
+#[derive(Debug, PartialEq, Eq, Clone, Copy)]
+struct PhysicalPageAddress(PhysicalAddress);
+
+impl TryFrom<PhysicalAddress> for PhysicalPageAddress {
+    type Error = InvalidAddressError;
+
+    fn try_from(addr: PhysicalAddress) -> Result<Self, Self::Error> {
+        if addr.is_page_aligned() {
+            Ok(Self(addr))
+        } else {
+            Err(InvalidAddressError)
+        }
+    }
+}
+
+#[derive(Debug, PartialEq, Eq, Clone, Copy)]
 pub enum PagePermissions {
     Read,
     ReadWrite,
@@ -45,96 +72,39 @@ pub enum PagePermissions {
     ReadWriteExecute,
 }
 
-#[repr(transparent)]
-#[derive(Debug, Clone, Copy)]
-pub struct PageTableEntry(u64);
+bitfield! {
+    #[derive(PartialEq, Eq, Clone, Copy)]
+    pub struct PageTableEntry(u64);
+    impl Debug;
+    pub valid, set_valid: 0;
+    // Permission bits are private since some combinations are reserved.
+    // The bitfield crate doesn't seem to support different visibility for getters and setters,
+    // so they are set to private here and public wrappers are defined for the getters.
+    readable_, set_readable: 1;
+    writable_, set_writable: 2;
+    executable_, set_executable: 3;
+    pub user, set_user: 4;
+    pub global, set_global: 5;
+    pub accessed, set_accessed: 6;
+    pub dirty, set_dirty: 7;
+    pub u64, ppn, set_ppn: 10, 53;
+}
 
 impl PageTableEntry {
-    const VALID_BIT: u64 = 1 << 0;
-    const READABLE_BIT: u64 = 1 << 1;
-    const WRITABLE_BIT: u64 = 1 << 2;
-    const EXECUTABLE_BIT: u64 = 1 << 3;
-    const USER_BIT: u64 = 1 << 4;
-    const GLOBAL_BIT: u64 = 1 << 5;
-    const ACCESSED_BIT: u64 = 1 << 6;
-    const DIRTY_BIT: u64 = 1 << 7;
-    const PPN_OFFSET: i32 = 10;
-
     pub const fn new_invalid() -> Self {
         Self(0)
     }
 
-    pub fn new_leaf(
-        ppn: PhysicalPageNumber,
-        permissions: PagePermissions,
-        user: bool,
-        global: bool,
-    ) -> Self {
-        use PagePermissions::*;
-        let mut bits = u64::from(ppn) << Self::PPN_OFFSET
-            | Self::VALID_BIT
-            | Self::ACCESSED_BIT
-            | Self::DIRTY_BIT;
-        bits |= match permissions {
-            Read => Self::READABLE_BIT,
-            ReadWrite => Self::READABLE_BIT | Self::WRITABLE_BIT,
-            Execute => Self::EXECUTABLE_BIT,
-            ReadExecute => Self::READABLE_BIT | Self::EXECUTABLE_BIT,
-            ReadWriteExecute => Self::READABLE_BIT | Self::WRITABLE_BIT | Self::EXECUTABLE_BIT,
-        };
-        if user {
-            bits |= Self::USER_BIT;
-        }
-        if global {
-            bits |= Self::GLOBAL_BIT;
-        }
-        Self(bits)
+    pub fn readable(&self) -> bool {
+        self.readable_()
     }
 
-    pub fn new_non_leaf(ppn: PhysicalPageNumber, global: bool) -> Self {
-        let mut bits = u64::from(ppn) << Self::PPN_OFFSET
-            | Self::VALID_BIT
-            | Self::ACCESSED_BIT
-            | Self::DIRTY_BIT;
-        if global {
-            bits |= Self::GLOBAL_BIT;
-        }
-        Self(bits)
+    pub fn writable(&self) -> bool {
+        self.writable_()
     }
 
-    pub fn is_valid(self) -> bool {
-        self.0 & Self::VALID_BIT != 0
-    }
-
-    pub fn physical_page_number(self) -> PhysicalPageNumber {
-        PhysicalPageNumber::try_from(
-            (self.0 >> Self::PPN_OFFSET) & ((1 << PHYSICAL_PAGE_NUMBER_BITS) - 1),
-        )
-        .unwrap()
-    }
-
-    pub fn is_leaf(self) -> bool {
-        self.0 & (Self::READABLE_BIT | Self::WRITABLE_BIT | Self::EXECUTABLE_BIT) != 0
-    }
-
-    pub fn is_readable(self) -> bool {
-        self.0 & Self::READABLE_BIT != 0
-    }
-
-    pub fn is_writable(self) -> bool {
-        self.0 & Self::WRITABLE_BIT != 0
-    }
-
-    pub fn is_executable(self) -> bool {
-        self.0 & Self::EXECUTABLE_BIT != 0
-    }
-
-    pub fn is_user(self) -> bool {
-        self.0 & Self::USER_BIT != 0
-    }
-
-    pub fn is_global(self) -> bool {
-        self.0 & Self::GLOBAL_BIT != 0
+    pub fn executable(&self) -> bool {
+        self.executable_()
     }
 }
 
@@ -152,11 +122,5 @@ impl Index<usize> for PageTable {
 
     fn index(&self, index: usize) -> &Self::Output {
         &self.0[index]
-    }
-}
-
-impl IndexMut<usize> for PageTable {
-    fn index_mut(&mut self, index: usize) -> &mut Self::Output {
-        &mut self.0[index]
     }
 }
